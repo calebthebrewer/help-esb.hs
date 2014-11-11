d552c0e1884c4e90693d73ece7ea3e8f4e18242d
Alexander Martin <alex@suitupalex.com>
2014-11-10 18:13:31 -0600

    Prepped files for Hackage.

--- HelpEsbClient.hs
+++ HelpEsbClient.hs
@@ -1,27 +1,34 @@
 {-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 
--- Haskell version of the Help.com ESB Client
--- v0.1.1
-
-module HelpEsbClient
--- Classes
-( EsbSend
+{-|
+Module : HelpEsbClient
+Description: Haskell version of the Help.com ESB Client
+Copyright: (c) Help.com, LLC, 2014
+License: MIT
+Maintainer: alex.martin@help.com
+Stability: Stable
+Portability: UNIX
+-}
+module HelpEsbClient (
+-- * Classes
+  EsbSend
 , EsbRecieve
--- Raw Exports
+-- * Raw Exported Functions
 , getSocket
 , sendSocketData
 , readSocketDataRaw
 , readSocketData
--- ESB Functions
+-- * ESB Functions
 , esbSend
 , esbRecieve
 , esbInit
 , esbListen
--- Utility
+-- * Utility Functions
 , encode
 , decode
 , eitherDecode
 , logger
+-- * Utility Types
 , Socket
 , module Data.UUID
 , module Data.UUID.V4
@@ -49,44 +56,70 @@ import qualified JSON.API.EventGroup.Post.Request as EventGroup.Post.Request
 import qualified JSON.API.Event.Post.Request as Event.Post.Request
 
 -- Classes
+-- | The 'EsbSend' class determines how a message should be sent to the ESB.
 class EsbSend a where
-  esbSend :: Socket -> a -> IO ()
+  -- | The 'esbSend' method takes a socket and writes somekind of payload.
+  esbSend :: Socket -- ^ The socket connection.
+    -> a -- ^ The payload.
+    -> IO () -- ^ Any IO output.
 
+-- | The 'EsbRecieve' class determines how a message from the ESB should be
+-- recieved.
 class EsbRecieve a where
-  esbRecieve :: Socket -> a -> IO ()
+  -- | The 'esbRecieve' method takes a socket and reads somekind of payload.
+  esbRecieve :: Socket -- ^ The socket connection.
+    -> a -- ^ The payload.
+    -> IO () -- ^ Any IO output.
 
--- Logging
-logger :: String -> IO ()
+-- | The 'logger' function simply logs out in a consistent way. Will be
+-- updated to be more robust.
+logger :: String -- ^ Messaged to be logged.
+  -> IO () -- ^ IO output.
 logger out = do
   let logPrefix = "# "
   putStrLn $ logPrefix ++ out
 
 -- Socket Specific Functions
-getSocket :: String -> Int -> IO Socket
+-- | The 'getSocket' function takes a host and port and connects to and
+-- returns the socket.
+getSocket :: String -- ^ Host address.
+  -> Int -- ^ Host port.
+  -> IO Socket -- ^ The socket connection.
 getSocket host port = withSocketsDo $ do
   (serveraddr:_) <- getAddrInfo Nothing (Just host) (Just (show port))
   sock <- socket (addrFamily serveraddr) Stream defaultProtocol
   connect sock (addrAddress serveraddr) >> return sock
 
-sendSocketData :: Socket -> C.ByteString -> IO ()
+-- | The 'sendSocketData' function accepts a socket and bytes, converts the
+-- bytes to cleaned up JSON, and writes the JSON to the socket.
+sendSocketData :: Socket -- ^ The socket connection.
+  -> C.ByteString -- ^ The JSON bytestring payload.
+  -> IO () -- ^ Any IO output.
 sendSocketData sock bytes = do
   let json = replace "h_" "" (C.unpack bytes)
   send sock (json ++ "\n")
   logger ("+ Raw Send: " ++ json)
 
-readSocketDataRaw :: Socket -> IO [Char]
+-- | The 'readSocketDataRaw' function accepts a socket and grabs whatever
+-- data might be in the latest message.
+readSocketDataRaw :: Socket -- ^ The socket connection.
+  -> IO [Char] -- ^ Any IO output.
 readSocketDataRaw sock = do
   message <- recv sock 1024
   logger ("+ Raw Read: " ++ message)
   return message
 
-readSocketData :: Socket -> IO (C.ByteString)
+-- | The 'readSocketData' function accepts a socket, reads and cleans up
+-- any JSON for parsing, and returns the bytes of JSON.
+readSocketData :: Socket -- ^ The socket connection.
+  -> IO (C.ByteString) -- ^ The JSON bytestring payload.
 readSocketData sock = do
   raw <- readSocketDataRaw sock
   let fixed = replace ",\"" ",\"h_" (replace "{\"" "{\"h_" raw)
   return $ C.pack fixed
 
 -- Send Socket Instances
+-- | The 'EsbSend' instance for a login request.
 instance EsbSend Login.Request.Data where
   esbSend sock payload = do
     uuid <- nextRandom
@@ -101,6 +134,7 @@ instance EsbSend Login.Request.Data where
     sendSocketData sock (encode message)
     logger ("Login Request: " ++ show message)
 
+-- | The 'EsbSend' instance for an Event Group post request.
 instance EsbSend EventGroup.Post.Request.Data where
   esbSend sock payload = do
     uuid <- nextRandom
@@ -116,6 +150,7 @@ instance EsbSend EventGroup.Post.Request.Data where
     sendSocketData sock (encode message)
     logger ("EventGroup API Request: " ++ show message)
 
+-- | The 'EsbSend' instance for an Event post request.
 instance EsbSend Event.Post.Request.Data where
   esbSend sock payload = do
     uuid <- nextRandom
@@ -132,6 +167,7 @@ instance EsbSend Event.Post.Request.Data where
     logger ("Event API Request: " ++ show message)
 
 -- Recieve Socket Instances
+-- | The 'EsbRecieve' instance for a Login response.
 instance EsbRecieve Login.Response.Message where
   esbRecieve sock message = do
     let payload = Login.Response.h_meta message
@@ -140,7 +176,13 @@ instance EsbRecieve Login.Response.Message where
       _ -> error "! Failed to login!"
 
 -- Initialization Instances
-esbInit :: Text -> [Text] -> String -> Int -> IO Socket
+-- | The 'esbInit' function initializes the socket connection and logs
+-- into the ESB.
+esbInit :: Text -- ^ Group name.
+  -> [Text] -- ^ Subscriptions.
+  -> String -- ^ Host address.
+  -> Int -- ^ Host port.
+  -> IO Socket -- ^ The socket connection.
 esbInit name subscriptions host port = do
   sock <- getSocket host port
   let loginData = Login.Request.Data { Login.Request.h_name = name, Login.Request.h_subscriptions = subscriptions }
@@ -148,7 +190,10 @@ esbInit name subscriptions host port = do
   return sock
 
 -- Essential Listening Logic
-esbListen :: Socket -> IO (C.ByteString)
+-- | The 'esbListen' function performs all essential listening logic
+-- for any ESB client.
+esbListen :: Socket -- ^ The socket connection.
+  -> IO (C.ByteString) -- ^ The JSON bytestring payload.
 esbListen sock = do
   bytes <- readSocketData sock
 
